637fc9c2156f59e131ab32f5025f9a25fc08a827
Winfred Lu <winfred.lu@gmail.com>
2014-07-01 14:52:16 +0800

    Enable BDU of LSM303DLHC Accelerometer to ensure both MSB and LSB data were updated.

--- Drivers/BSP/STM32F401-Discovery/stm32f401_discovery_accelerometer.c
+++ Drivers/BSP/STM32F401-Discovery/stm32f401_discovery_accelerometer.c
@@ -34,18 +34,18 @@
   * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   *
   ******************************************************************************
-  */ 
+  */
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f401_discovery_accelerometer.h"
 
 /** @addtogroup BSP
   * @{
-  */ 
+  */
 
 /** @addtogroup STM32F401_DISCOVERY
   * @{
-  */ 
+  */
 
 /** @addtogroup STM32F401_DISCOVERY_ACCELERO
   * @{
@@ -55,7 +55,7 @@
 /** @defgroup STM32F401_DISCOVERY_ACCELERO_Private_TypesDefinitions STM32F401_DISCOVERY_ACCELERO_Private_TypesDefinitions
   * @{
   */
-  
+
 /**
   * @}
   */
@@ -74,11 +74,11 @@
 
 /**
   * @}
-  */ 
-  
+  */
+
 /** @defgroup STM32F401_DISCOVERY_ACCELERO_Private_Variables STM32F401_DISCOVERY_ACCELERO_Private_Variables
   * @{
-  */ 
+  */
 static ACCELERO_DrvTypeDef *AccelerometerDrv;
 
 /**
@@ -108,7 +108,7 @@ uint8_t BSP_ACCELERO_Init(void)
   uint16_t ctrl = 0x0000;
   ACCELERO_InitTypeDef LSM303DLHC_InitStructure;
   ACCELERO_FilterConfigTypeDef LSM303DLHC_FilterStructure={0,0,0,0};
-  
+
   if(Lsm303dlhcDrv.ReadID() == I_AM_LMS303DLHC)
   {
     /* Initialize the gyroscope driver structure */
@@ -120,26 +120,26 @@ uint8_t BSP_ACCELERO_Init(void)
     LSM303DLHC_InitStructure.AccOutput_DataRate = LSM303DLHC_ODR_50_HZ;
     LSM303DLHC_InitStructure.Axes_Enable= LSM303DLHC_AXES_ENABLE;
     LSM303DLHC_InitStructure.AccFull_Scale = LSM303DLHC_FULLSCALE_2G;
-    LSM303DLHC_InitStructure.BlockData_Update = LSM303DLHC_BlockUpdate_Continous;
+    LSM303DLHC_InitStructure.BlockData_Update = LSM303DLHC_BlockUpdate_Single;
     LSM303DLHC_InitStructure.Endianness=LSM303DLHC_BLE_LSB;
     LSM303DLHC_InitStructure.High_Resolution=LSM303DLHC_HR_ENABLE;
-    
+
     /* Configure MEMS: data rate, power mode, full scale and axes */
     ctrl |= (LSM303DLHC_InitStructure.Power_Mode | LSM303DLHC_InitStructure.AccOutput_DataRate | \
                        LSM303DLHC_InitStructure.Axes_Enable);
-    
+
     ctrl |= ((LSM303DLHC_InitStructure.BlockData_Update | LSM303DLHC_InitStructure.Endianness | \
                       LSM303DLHC_InitStructure.AccFull_Scale | LSM303DLHC_InitStructure.High_Resolution) << 8);
-    
+
     /* Configure the accelerometer main parameters */
     AccelerometerDrv->Init(ctrl);
-    
+
     /* Fill the accelerometer LPF structure */
     LSM303DLHC_FilterStructure.HighPassFilter_Mode_Selection =LSM303DLHC_HPM_NORMAL_MODE;
     LSM303DLHC_FilterStructure.HighPassFilter_CutOff_Frequency = LSM303DLHC_HPFCF_16;
     LSM303DLHC_FilterStructure.HighPassFilter_AOI1 = LSM303DLHC_HPF_AOI1_DISABLE;
     LSM303DLHC_FilterStructure.HighPassFilter_AOI2 = LSM303DLHC_HPF_AOI2_DISABLE;
-    
+
     /* Configure MEMS: mode, cutoff frquency, Filter status, Click, AOI1 and AOI2 */
     ctrl = (uint8_t) (LSM303DLHC_FilterStructure.HighPassFilter_Mode_Selection |\
                       LSM303DLHC_FilterStructure.HighPassFilter_CutOff_Frequency|\
@@ -169,11 +169,11 @@ void BSP_ACCELERO_Reset(void)
   if(AccelerometerDrv->Reset != NULL)
   {
     AccelerometerDrv->Reset();
-  }  
+  }
 }
 
 /**
-  * @brief  Config Accelerometer click IT 
+  * @brief  Config Accelerometer click IT
   * @param  None
   * @retval None
   */
@@ -187,7 +187,7 @@ void BSP_ACCELERO_Click_ITConfig(void)
 
 /**
   * @brief  Get XYZ acceleration
-  * @param pDataXYZ Pointeur on 3 angular accelerations 
+  * @param pDataXYZ Pointeur on 3 angular accelerations
   *                 pDataXYZ[0] = X axis, pDataXYZ[1] = Y axis, pDataXYZ[2] = Z axis
   * @retval None
   */
@@ -200,31 +200,31 @@ void BSP_ACCELERO_GetXYZ(int16_t* pDataXYZ)
     AccelerometerDrv->GetXYZ(pDataXYZ);
 
     if(AccelerometerDrv == &Lsm303dlhcDrv)
-    { 
+    {
       SwitchXY  = pDataXYZ[0];
       pDataXYZ[0] = pDataXYZ[1];
       /* Invert Y Axis to be conpliant with LIS3DSH */
       pDataXYZ[1] = -SwitchXY;
-    } 
+    }
   }
 }
 
 
 /**
   * @}
-  */ 
+  */
 
 /**
   * @}
-  */ 
-  
-/**
-  * @}
-  */ 
+  */
 
 /**
   * @}
-  */ 
-  
+  */
 
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/     
+/**
+  * @}
+  */
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
