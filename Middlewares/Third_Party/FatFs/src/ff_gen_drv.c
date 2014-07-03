d520f146e5c4606d2070ab2dcc61a5f23dd27c3a
Winfred Lu <winfred.lu@gmail.com>
2014-07-03 11:02:51 +0800

    Remove the GCC missing-braces warning.
    This is GCC bug #53119:
    http://gcc.gnu.org/bugzilla/show_bug.cgi?id=53119
    
    GCC warns when the first member in a struct is an array.
    I work around it with double brace pairs, the outer for the struct and the inner
    for the array.
    
    Compiling messages:
    Middlewares/Third_Party/FatFs/src/ff_gen_drv.c:34:1: warning: missing braces
    around initializer [-Wmissing-braces]
     Disk_drvTypeDef  disk = {0};
     ^

--- Middlewares/Third_Party/FatFs/src/ff_gen_drv.c
+++ Middlewares/Third_Party/FatFs/src/ff_gen_drv.c
@@ -1,6 +1,6 @@
 /**
   ******************************************************************************
-  * @file    ff_gen_drv.c 
+  * @file    ff_gen_drv.c
   * @author  MCD Application Team
   * @version V1.0.0
   * @date    18-February-2014
@@ -16,14 +16,14 @@
   *
   *        http://www.st.com/software_license_agreement_liberty_v2
   *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   * See the License for the specific language governing permissions and
   * limitations under the License.
   *
   ******************************************************************************
-  */ 
+  */
 
 /* Includes ------------------------------------------------------------------*/
 #include "ff_gen_drv.h"
@@ -31,17 +31,17 @@
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-Disk_drvTypeDef  disk = {0};
+Disk_drvTypeDef  disk = {{0}};
 
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
 
 /**
   * @brief  Links a compatible diskio driver and increments the number of active
-  *         linked drivers.          
+  *         linked drivers.
   * @note   The number of linked drivers (volumes) is up to 10 due to FatFs limits
   * @param  drv: pointer to the disk IO Driver structure
-  * @param  path: pointer to the logical drive path 
+  * @param  path: pointer to the logical drive path
   * @retval Returns 0 in case of success, otherwise 1.
   */
 uint8_t FATFS_LinkDriver(Diskio_drvTypeDef *drv, char *path)
@@ -50,7 +50,7 @@ uint8_t FATFS_LinkDriver(Diskio_drvTypeDef *drv, char *path)
   uint8_t DiskNum = 0;
   if(disk.nbr <= _VOLUMES)
   {
-    disk.drv[disk.nbr] = drv;  
+    disk.drv[disk.nbr] = drv;
     DiskNum = disk.nbr++;
     path[0] = DiskNum + '0';
     path[1] = ':';
@@ -64,16 +64,16 @@ uint8_t FATFS_LinkDriver(Diskio_drvTypeDef *drv, char *path)
 /**
   * @brief  Unlinks a diskio driver and decrements the number of active linked
   *         drivers.
-  * @param  path: pointer to the logical drive path  
+  * @param  path: pointer to the logical drive path
   * @retval Returns 0 in case of success, otherwise 1.
   */
 uint8_t FATFS_UnLinkDriver(char *path)
-{ 
+{
   uint8_t DiskNum = 0;
   uint8_t ret = 1;
-  
+
   if(disk.nbr >= 1)
-  {    
+  {
     DiskNum = path[0] - '0';
     if(DiskNum <= disk.nbr)
     {
@@ -81,7 +81,7 @@ uint8_t FATFS_UnLinkDriver(char *path)
       ret = 0;
     }
   }
-  
+
   return ret;
 }
 
@@ -94,6 +94,5 @@ uint8_t FATFS_GetAttachedDriversNbr(void)
 {
   return disk.nbr;
 }
- 
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
 
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
